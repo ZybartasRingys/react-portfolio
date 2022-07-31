b6cd25faa0c644c192751699a6cd8e00d072c3c6
Zybartas Ringys <developerzr92@gmail.com>
2022-07-31 11:44:24 +0300

    netlify

--- src/components/Projects/Card.js
+++ src/components/Projects/Card.js
@@ -1,46 +1,24 @@
 import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFile, faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 
 function Card() {
-  return (
-    <div className="card">
-      <div className="links">
-        <Link to="/about">
-          <FontAwesomeIcon
-            icon={faFile}
-            className="card-icon"
-            color="#1ba098"
-          />
-        </Link>
+  const baseURL = 'https://github.com/ZybartasRingys/ecommerce'
+  const [projects, setProjects] = useState([])
 
-        <Link to="/about">
-          <FontAwesomeIcon
-            icon={faUpRightFromSquare}
-            className="card-icon"
-            color="#1ba098"
-          />
-        </Link>
-      </div>
-      <div className="card-text">
-        <h3 className="project-title">
-          Modern Full Stack ECommerce React Application with Stripe Sanity IO
-        </h3>
-        <p className="project-desc">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam dolore
-          deserunt libero porro facere, dolor sequi omnis dolorem cupiditate
-          nobis quia aliquam praesentium itaque aperiam similique nostrum modi
-          maiores quas.
-        </p>
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      {
+        console.log(response)
+      }
+    })
+  }, [])
 
-        <div className="project-info">
-          <p className="project-stack">REACT SANITY.IO HEADLESS CMS STRIPE</p>
-        </div>
-      </div>
-    </div>
-  )
+  return <div></div>
 }
 
 export default Card
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,16 +1,20 @@
 import './index.scss'
 import Card from './Card'
+import Loader from 'react-loaders'
 
 const Projects = () => {
   return (
-    <div className="container projects-page">
-      <div className="text-zone">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+    <>
+      <div className="container projects-page">
+        <div className="text-zone">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
