a15092946ade3d111b00c82deb88d1423c4b6407
Zybartas Ringys <developerzr92@gmail.com>
2022-07-31 11:47:49 +0300

    commit

--- src/components/Projects/Card.js
+++ src/components/Projects/Card.js
@@ -1,23 +1,6 @@
 import React from 'react'
-import axios from 'axios'
-import { useState, useEffect } from 'react'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFile, faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
 
 function Card() {
-  const baseURL = 'https://github.com/ZybartasRingys/ecommerce'
-  const [projects, setProjects] = useState([])
-
-  useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      {
-        console.log(response)
-      }
-    })
-  }, [])
-
   return <div></div>
 }
 
